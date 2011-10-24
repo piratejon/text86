1749ae5fa20a064682119e35acbc46ef26a5e67f
Jonathan Wesley Stone <jonathanwesleystone@gmail.com>
2011-10-23 21:48:31 -0500

    a bunch of changes, i have no idea what

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 
 compile:
 	nasm keyboard_sample.asm
+#	ndisasm keyboard_sample -o 0x7c00 | cut --complement -c9-26 > disasm
+	ndisasm keyboard_sample -o 0x7c00 > disasm
+#	mousepad disasm
 
 run:
 	bochs -f ./testing
--- keyboard_sample.asm
+++ keyboard_sample.asm
@@ -4,27 +4,151 @@ top:
 use16
 org 0x7C00
 
+; FAT12
+  jmp short 0x3c
+  nop
+OEM: db 'WoDoS1.0'
+bytes_per_sector:     dw 0x0200
+sectors_per_cluster:  db 0x01
+reserved_sectors:     dw 0x0002
+number_of_fats:       db 0x02
+number_of_dirs:       dw 0x01
+total_sectors:        dw 0x0000
+media_descriptor_type:db 0xf0
+
+; turn this back on after we are ready
+  cli
+
+; where our records will be stored
+  lea di, [mem_map]
+
+; find all the ram -- from http://wiki.osdev.org/How_Do_I_Determine_The_Amount_Of_RAM#Getting_an_E820_Memory_Map
+  xor ebx, ebx
+  push bx
+  pop es
+  xor bp, bp
+  mov edx, 0x0534d4150
+  mov eax, 0xe820
+  mov [es:di+20], dword 1
+  mov ecx, 24
+  int 0x15
+  jc short .failed
+  mov edx, 0x0534d4150
+  cmp eax, edx
+  jne short .failed
+  test ebx, ebx
+  je short .failed
+  jmp short .jmpin
+.e820lp:
+  mov eax, 0xe820
+  mov [es:di+20], dword 1
+  mov ecx, 24
+  int 0x15
+  jc short .e820f
+  mov edx, 0x0534d4150
+.jmpin:
+  jcxz .skipent
+
+  ; keep track of the largest memory
+  cmp byte [es:di+20], 1
+  jg .notbigger
+  mov eax, [es:di+8]
+  cmp eax, [biggest_chunk_size]
+  jge .notbigger
+  mov [biggest_chunk_size], eax
+  mov eax, [es:di]
+  mov [biggest_chunk_location], eax
+.notbigger:
+
+  cmp cl, 20
+  jbe short .notext
+  test byte [es:di+20], 1
+  je short .skipent
+.notext:
+  mov ecx, [es:di+8]
+  or ecx, [es:di+12]
+  jz .skipent
+  inc bp
+  add di, 24
+.skipent:
+  test ebx, ebx
+  jne short .e820lp
+.e820f:
+  ; mov [mmap_ent], bp
+  ; bp has the count of entries
+  jmp start
+.failed:
+  mov si, e820_error
+  call write_si_zstr_to_video_memory
+  hlt
+
+start:
 ; all our string ops move forward
   cld
 
 ; initialize the keyboard interrupt handler
-  cli
   xor ax, ax
   push ax
   pop ds
   mov word[ds:(9*4)], keyboard_handler
   mov word[ds:(9*4)+2], 0
 
-; reset the screen b800:0-b800:7c0, that's b8000-b87c0
+; reset the screen b800:0-b800:7c0, that's b8000-b87cf
   push 0xb800
   pop es
   xor di,di
   mov cx, 2000
-  mov ax, 0x7820
+  ; mov ax, 0x7820 ; inverse from the default blank
+  mov ax, 0x0720 ; default blank screen
   repnz stosw
+  xor di, di
+
+  hlt
+
+write_cx_bytes_to_video_memory:
+  mov al, [si]
+  inc si
+  call write_al_to_video_memory
+  loop write_cx_bytes_to_video_memory
+
+write_si_zstr_to_video_memory:
+  mov al, [si]
+  test al, al
+  jz .end
+  inc si
+  stosb
+  inc di
+  jmp write_si_zstr_to_video_memory
+.end:
+  ret
+
+write_al_to_video_memory:
+  push ax
+  push ax
+  shr al, 4
+  and al, 0x0f
+  cmp al, 0x09
+  jle .num
+  add al, 0x07
+.num:
+  add al, 0x30
+  stosb
+  inc di
+  pop ax
+  and al, 0x0f
+  cmp al, 0x09
+  jle .num2
+  add al, 0x07
+.num2:
+  add al, 0x30
+  stosb
+  inc di
+  pop ax
+
+  ret
 
 ; initialize the buffer and index
-  mov cx, buffer
+  mov cx, pbuffer
   shr cx, 4
   mov es, cx
   xor di, di
@@ -114,7 +238,7 @@ keyboard_handler:
   push es
   pop ds
   pop es
-  mov cx, 2000
+  mov cx, 1920
   repnz movsw
   pop di ; restore the buffer index
   pop cx ; restore the shift flag
@@ -131,13 +255,22 @@ keyboard_handler:
 
 iret
 
+e820_error: db 'E820',0
+
 qwerty_ascii_lower: db 0,0,'1','2','3','4','5','6','7','8','9','0','-','=',0,0,'q','w','e','r','t','y','u','i','o','p','[',']',0,0,'a','s','d','f','g','h','j','k','l', 0x3b, 0x27, '`', 0, '\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0, 0, 0, ' '
 qwerty_ascii_upper: db 0,0,0x21,'@',0x23,'$','%','^','&','*','(',')','_','+',0,0,'Q','W','E','R','T','Y','U','I','O','P','{','}',0,0,'A','S','D','F','G','H','J','K','L', ':', '"', '~', 0, 0x7c, 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 0, 0, 0, ' '
 
 times 510-($-$$) db 0
 dw 0xAA55
 
-buffer:
+pbuffer: dw 0 ; pointer to the buffer in memory
+pcursor: dw 0 ; cursor offset from start of buffer
+pwindow: dw 0 ; address of first character to display at 0xb800, for scrolling
+
+biggest_chunk_location: dw 0
+biggest_chunk_size: dw 0
+
+mem_map:
 
 times 1474560 - ($ - $$) db 0x20 ; our "filesystem"
 
