f7d0f0a6ed4aaa340bfd9f392ee72666b8bca39e
Jonathan Wesley Stone <jonathanwesleystone@gmail.com>
2011-11-04 01:38:18 -0500

    stopped working on the netbook?

--- reboot.asm
+++ reboot.asm
@@ -9,6 +9,8 @@ org 0x7c00
 
 cli ; we are getting ready don't let anyone interrupt us
 
+mov [boot_dev], dl ; save boot device so we don't have to write a USB driver!
+
 ; initialize the buffer for the memory spec
 push dword 0
 pop ds
@@ -57,13 +59,13 @@ e820_loop:
   ; top left character of the screen
   mov di, 0x0
 
-  ;mov eax, [large_window_lo]
-  ;call hexprint_eax
-  ;mov al, '@'
-  ;stosb
-  ;inc di
-  ;mov eax, [relocate]
-  ;call hexprint_eax
+  mov eax, [large_window_lo]
+  call hexprint_eax
+  mov al, '@'
+  stosb
+  inc di
+  mov eax, [relocate]
+  call hexprint_eax
 
 ;  jmp short await_keypress
 ;  hlt
@@ -74,42 +76,46 @@ e820_loop:
   and al, 0xfe
   out 0x92, al
 
-; now it is time to scan the PCI bus
-; bus is a 8-bit number, device is a 5 bit number, function is a 3 bit number
-; device must exist for a function to exist
-pci_enum:
-  xor cx, cx
-.pci_enum_loop:
-  ; create the CONFIG_ADDRESS register
-  movzx eax, cx
-  inc cx
-  shl eax, 10
-  or eax, 0x80000008
+  push di
 
-  mov ebx, eax
+; get boot device parameters
+  mov ah, 8
+  mov dl, [boot_dev]
+  push dword 0
+  pop es
+  pop di
+  int 0x13 ; ah=8, dl=boot_dev; get drive parameters
+  cmp ah, 0
+  jnz short fail_int13
 
-  mov dx, 0xcf8
-  out dx, eax
-  mov dx, 0xcfc
-  in eax, dx
-  cmp ax, 0xffff
-  je .next
+  ; try reading the first sector
+  push word 0
+  pop es
+  mov bx, relocate
+  mov ax, 0x0201 ; ah is int13h function, al is # sectors
+  ;xor cx, cx ; ch=track, cl=sector
+  mov cx, 0x0101
+  mov dh, 0 ; head
+  mov dl, [boot_dev]
+  int 0x13
 
-  ; we have a valid PCI device, is it the needed type?
-  ror eax, 16
-  cmp ax, 0x0c03
-  jne .next
+  push 0xb800
+  pop es
+  pop di
 
-;  push eax
-;  mov eax, ebx
-;  call hexprint_eax
-;  pop eax
-;  call hexprint_eax
-;  add di, 160-(8*2*2)
+  add di, 2
 
-.next:
-  cmp cx, 0x2000
-  jl .pci_enum_loop
+  and eax, 0x0000ffff
+  call hexprint_eax
+  
+  jc short fail_int13
+
+  jmp short await_keypress
+
+fail_int13:
+  mov si, fail_int13_prompt
+  call zprint
+  jmp short await_keypress
  
 ; await a keypress, then reboot
 await_keypress:
@@ -199,10 +205,13 @@ memmap: db 'memmap ', 0
 failed: db 'failed ', 0
 found: db 'found ', 0
 prompt: db 'press any key to reboot!', 0
+fail_int13_prompt: db 'boot device not found', 0
 e820_error: db 'e820 err', 0
 
 times 510 - ($ - $$) db 0x00 ; round out the boot sector
 dw 0xaa55
+
+boot_dev: db 0
 
 relocate: dd 0
 large_window_size:
@@ -219,34 +228,3 @@ di_size_hi: dd 0
 di_flags: dq 0
 e820_buf_end:
 
-pci_memory:
-device_id: dw 0
-vendor_id: dw 0
-status: dw 0
-command: dw 0
-class_id: db 0
-subclass: db 0
-prog_if: db 0
-revision_id: db 0
-bist: db 0
-header_type: db 0
-latency_timer: db 0
-cache_line_size: db 0
-bar0: dd 0
-bar1: dd 0
-bar2: dd 0
-bar3: dd 0
-bar4: dd 0
-bar5: dd 0
-cardbus_cis_p: dd 0
-subsystem_id: dw 0
-subsystem_vendor_id: dw 0
-expansion_rom: dd 0
-reserved1: db 0,0,0
-capabilites: db 0
-reserved2: dd 0
-max_latency: db 0
-min_grant: db 0
-interrupt_pin: db 0
-interrupt_line: db 0
- 
