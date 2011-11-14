afc7589e32fb9deb34a94b68f476b52ad9ad747e
Jonathan Wesley Stone <jonathanwesleystone@gmail.com>
2011-11-14 13:21:29 -0600

    remove some cruft, not going in the fat direction for now

--- reboot.asm
+++ reboot.asm
@@ -2,42 +2,18 @@ top:
 bits 16
 org 0x7c00
 
-FAT_HEADER:
-.jmp: jmp short .boot_code
-.nop: nop
-.oem: db 'roflmfao' ; 64 bits of ascii flavored botulism, TODO: ABUSE ME!
-.bytes_per_sector: dw 0x200 ; 512 byte sectors
-.sectors_per_cluster: db 4 ; sectors per cluster
-.reserved_sectors: dw 2 ; reserved sectors -- the boot sector and kernel, i guess?
-.number_of_fats: db 2 ; number of FATs lol
-.number_of_dirents: dw 0x200 ; number of directory entries?
-.sectors_in_volume: dw 0x100 ; sectors in volume
-.media_descriptor_type: db 0xf8 ; media descriptor type
-.sectors_per_fat: dw 1 ; sectors per fat
-.sectors_per_track: dw 0x20 ; sectors per track
-.heads: dw 0x40 ; heads
-.hidden_sectors: dd 0 ; hidden_sectors ; TODO Abuse me?
-.large_sectors: dd 0 ; large_sectors ; TODO Abuse me?
-.drive_number: db 0 ; drive_number
-.signature: db 0x29 ; signature
-.serial: dd 0 ; volume serial, ignorable ;TODO: ABUSE ME!
-.volume_label: db 'volumelabel' ; space-padded volume label, length 11 ; TODO: ABUSE ME!
-.system_id: dd 0xdeadc0d3, 0xc0c0daff; sysid, ignored as untrusted ; TODO: ABUSE ME!
-.boot_code:
-
 boot_code:
 
 cli ; we are getting ready don't let anyone interrupt us
 
-push 0
-pop es
-push 0
-pop ds
+xor di, di
+mov ds, di
+mov es, di
 
 mov [boot_dev], dl ; save boot device so we don't have to write a USB driver!
 
-mov ax, [0x463]
-mov [video_port], ax
+mov ax, [0x463] ; get video port address from bios data area
+mov [video_port], ax ; save video port address for later amusement
 
 ; first let's reset the disk subsystem just for fun
 mov ah, 0
@@ -49,9 +25,7 @@ jc int13_error
 ; now we can read info about the drive!
 mov ah, 8
 mov dl, [boot_dev]
-push word 0
-pop es
-xor di, di
+; es:di should already be 0:0
 int 0x13
 jc int13_error
 
@@ -75,7 +49,9 @@ mov [max_cylinder], dx
 ;mov dh, 0 ; head #, can be zero
 ;mov dl, [boot_dev]
 
-mov eax, 1 ; sector zero is loaded at 0x7c00 already!
+; the following two lines are two bytes less than mov eax, 1
+xor eax, eax
+inc ax ; sector zero is loaded at 0x7c00 already
 call lba_to_chs
 mov ah, 2 ; int13 function= read to memory
 mov al, 1 ; number of sectors to read
@@ -91,34 +67,11 @@ push 0xb800
 pop es
 xor di, di
 
-mov eax, [sector_2]
-call hexprint_eax
-
 ; now we should have a good keymap!
 
 jmp await_keypress
 
 lba_to_chs: ; converts zero-indexed LBA sector eax to int13 CHS
-; conversion formula:
-  ; sector = (lba % max_sector ) + 1
-  ; cylhead = ( lba / max_sector )
-  ; head = cylhead % (max_head+1)
-  ; cylinder = cylhead / (max_head+1)
-
-; registers:
-  ; ch = low 8 bits of cylinder #
-  ; cl bits 0-5: sector #
-  ; cl bits 6-7: high 2 bits of cylinder #
-  ; dh = head #
-
-; cwd/cdq sign-extends al/ax into dx:ax or edx:eax
-
-; idiv r/m
-  ; divides ah:al by r/m8, al=quotient, ah=remainder
-  ; divides dx:ax by r/m16, ax=quotient, dx=remainder
-  ; divides edx:eax by r/m32, eax=quotient, dx=remainder
-
-  push ebx
   cdq ; extend eax sign through edx so we can divide reasonably
   movsx ebx, byte [max_sector]
   idiv ebx
@@ -138,7 +91,6 @@ lba_to_chs: ; converts zero-indexed LBA sector eax to int13 CHS
   shl ah, 6 ; get the bits into the proper position
   ; and ah, 0xc0 ; not necessary because SHL clears the LSB
   or cl, ah
-  pop ebx
 
   ret
 
@@ -205,25 +157,6 @@ await_keypress:
   sti
   jmp short $
 
-
-boot_dev: db 0
-shift_flag: db 0
-max_sector: db 0
-max_head: db 0
-max_cylinder: dw 0
-video_port: dw 0
-last_vid_byte: dw 0
-
-BEGIN_OVERWRITE:
-times 444 - ($ - $$) db 0x00 ; round out the boot sector
-
-dw 0x0 ; why oughtn't i utilize this word? #OCCUPYBOOTSECTORS
-
-times 510 - ($ - $$) db 0xcd ; partition table
-dw 0xaa55
-
-sector_2:
-
 keyboard_handler:
 .spin: ; ahhahahahaha dizzy loop
   in al, 0x64
@@ -246,6 +179,8 @@ keyboard_handler:
   je short .shift_up
   cmp al, 0xb6
   je short .shift_up
+  cmp al, 0x1c
+  je short .crlf
 
   cmp al, qwerty_ascii_lower_end - qwerty_ascii_lower + 1
   jae short .next
@@ -270,7 +205,14 @@ keyboard_handler:
   jmp short .reset_cursor_to_di
 
 .crlf:
-  jmp short .next
+  push word 160
+  mov ax, di
+  cwd
+  idiv word [esp]
+  add esp, 2
+  sub di, dx
+  add di, 160
+  jmp short .reset_cursor_to_di
 
 .translate:
   mov bx, qwerty_ascii_upper
@@ -290,8 +232,8 @@ keyboard_handler:
 .reset_cursor_to_di:
   call reset_cursor_to_di
 
-  cmp di, 0x200
-  call write_buffer
+  ;cmp di, 0x200
+  ;call write_buffer
 
 .next:
   ; don't be lame and leave the brogrammable interrupt controller hangin'
@@ -327,22 +269,29 @@ reset_cursor_to_di:
   ret
 
 write_buffer:
-  ; write 0x200 bytes from es:0 to sector eax
-  mov eax, 2
-  call lba_to_chs
+  ; write 1 sector from es:bx to sector eax
+  push bx
+  ; eax is parameter
+  call lba_to_chs ; retain cl, ch, dh
   mov ah, 3
   mov al, 1
   mov dl, [boot_dev]
-  mov bx, 0
+  pop bx
   int 0x13
-  jc int13_error
+  ; carry flag set on error
   ret
 
-last_pos_written: dd 0
-                ; initial values for data
-last_cylinder: dw 0  ; 0
-last_head: dw 0 ; 0
-last_sector: dw 0  ; 2
+times 510 - ($ - $$) db 0xcd ; partition table
+dw 0xaa55
+
+sector_2:
+
+boot_dev: db 0
+shift_flag: db 0
+max_sector: db 0
+max_head: db 0
+max_cylinder: dw 0
+video_port: dw 0
 
 qwerty_ascii_lower: db 0,0,'1','2','3','4','5','6','7','8','9','0','-','=',0,0,'q','w','e','r','t','y','u','i','o','p','[',']',0,0,'a','s','d','f','g','h','j','k','l', 0x3b, 0x27, '`', 0, '\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0, 0, 0, ' '
 qwerty_ascii_lower_end:
